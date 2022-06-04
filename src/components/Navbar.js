2e921f7bff68f60b880acff514d895a7e54bf865
ChangeWithCode <fa17-bcs-199@cuiwah.edu.pk>
2022-06-04 10:55:10 +0500

    Test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,33 +9,33 @@ const Navbar = () => {
     return (  
 
 
-<nav class="navbar navbar-expand-lg bg-dark p-3 rounded-bottom  ">
-  <div class="container-lg">
-    <Link class="navbar-brand  link-light text-uppercase " to="/">Muhammad Qasim</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon "></span>
-    </button>
-    <div class="collapse navbar-collapse flex-grow-0  " id="navbarSupportedContent">
-      <ul class="navbar-nav  mx-auto text-uppercase " >
-        <li class="nav-item ">
-            <NavLink id="1" className="nav-link  text-white m-2" aria-current="page" to="/">Home</NavLink>
-        </li>
-        <li class="nav-item  ">
-          <NavLink id="2" className="nav-link  text-white m-2"   to="/about">About Us</NavLink>
-        </li>
-        <li class="nav-item ">
-          <NavLink id="3" className="nav-link  text-white m-2"  to="/services">Services</NavLink>
-        </li>
-        <li  id="4"class="nav-item ">
-          <NavLink className="nav-link  text-white m-2" to="/projects">Portfolios</NavLink>
-        </li>
-        <li id="5" class="nav-item ">
-          <NavLink className="nav-link  text-white m-2"  to="/contact">Contact Us</NavLink>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <nav class="navbar rounded-bottom navbar-expand-lg navbar-dark bg-dark">
+          <div class="container">
+          <Link class="navbar-brand link-dark text-uppercase " to="/">Muhammad Qasim</Link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#header01" aria-controls="header01" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="header01">
+            <ul class="navbar-nav mx-auto text-uppercase ">
+              <li class="nav-item ">
+                <NavLink id="1" className="nav-link  text-white m-2" aria-current="page" to="/">Home</NavLink>
+              </li>
+              <li class="nav-item  ">
+                <NavLink id="2" className="nav-link  text-white m-2" to="/about">About Us</NavLink>
+              </li>
+              <li class="nav-item ">
+                <NavLink id="3" className="nav-link  text-white m-2" to="/services">Services</NavLink>
+              </li>
+              <li id="4" class="nav-item ">
+                <NavLink className="nav-link  text-white m-2" to="/projects">Portfolios</NavLink>
+              </li>
+              <li id="5" class="nav-item ">
+                <NavLink className="nav-link  text-white m-2" to="/contact">Contact Us</NavLink>
+              </li>
+            </ul>
+            </div>
+          </div>
+        </nav>
 
 )}
  
